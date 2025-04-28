18e07b593c1be480bbf983a207cd731c07276cce
Vladimir Jaward Hancco Soncco <vhanccos@unsa.edu.pe>
2025-04-28 15:31:15 -0500

    move branch

--- anexo2/Demo.java
+++
@@ -1,9 +0,0 @@
-public class Demo {
-  public static void main(String[] args) {
-    CubbyHole cub = new CubbyHole();
-    Consumidor cons = new Consumidor(cub, 1);
-    Productor prod = new Productor(cub, 1);
-    prod.start();
-    cons.start();
-  }
-}
